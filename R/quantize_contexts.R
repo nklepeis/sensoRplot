b929c7f490ccfb12be7e781142ed9b47b49325c9
NK <dev@klepeis.net>
2023-12-26 12:11:22 -0800

    clean up context manip

--- R/quantize_contexts.R
+++ R/quantize_contexts.R
@@ -16,11 +16,22 @@
 #'
 #' @details
 #'
-#' "round" quantizes the context times to nearest time unit value
-#' "floor" quantizes the context times DOWN to the nearest unit boundary
-#' "ceiling" quantizes the context times UP to the nearest unit boundary
+#' * "round" quantizes the context times to nearest time unit value
+#' * "floor" quantizes the context times DOWN to the nearest unit boundary
+#' * "ceiling" quantizes the context times UP to the nearest unit boundary
+#'
+#' Note:  'Times' in the contexts objects need to be Time.POSIX objects
+#' or coerceable to meaningful times using 'lubridate::as_datetime'.
+#'
+#' If quantization of times results in repeated times for sequential
+#' contexts, the first context is included with others being discarded.
 #'
 #' @seealso lubridate::round_date
+#'
+#' @example
+#' df <- data.frame(Time=c(1.1,2.04,2.9,4.3,5.5,6.4),
+#'       GrpA=LETTERS[1:6],GrpB=LETTERS[6:11])
+#' quantize_contexts(df,unit="1 second")
 #'
 #------------------------------------------------------
 
@@ -32,9 +43,12 @@ quantize_contexts <- function (..., unit="10 seconds",
   contexts %>%
     map(function (x) {
       switch(method,
-        round = x %>% mutate(Time = round_date(unit=unit)),
-        floor = x %>% mutate(Time = round_date(unit=unit)),
-        ceiling = x %>% mutate(Time = round_date(unit=unit))
+        round = x %>% mutate(Time = round_date(as_datetime(Time), unit=unit)) %>%
+          distinct(Time, .keep_all = TRUE),
+        floor = x %>% mutate(Time = round_date(as_datetime(Time), unit=unit)) %>%
+          distinct(Time, .keep_all = TRUE),
+        ceiling = x %>% mutate(Time = round_date(as_datetime(Time), unit=unit)) %>%
+          distinct(Time, .keep_all = TRUE)
       )
     })
 
