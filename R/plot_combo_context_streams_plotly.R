42c24b07d6fd9f390c4434e58aa61a00bf5d8a01
NK <dev@klepeis.net>
2024-03-21 00:12:56 -0700

    fix typo

--- R/plot_combo_context_streams_plotly.R
+++ R/plot_combo_context_streams_plotly.R
@@ -68,7 +68,7 @@ plot_combo_contexts_streams_plotly <- function(contexts, streams,
   #  Timeline plot
   #fig2 <- plot_state_timeline2(timeline, collapse=collapse,
   #                             bg=bg, fg=fg, font.size=font.size)
-  fig2 <- plot_state_plotly(timeline, collapse=collapse,
+  fig2 <- plot_state_timeline_plotly(timeline, collapse=collapse,
                                bg=bg, fg=fg, font.size=font.size)
   fig2$x$config <- NULL # config is NULL for timeline subplot
 
